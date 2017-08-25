f8b7c15b1cf457ce093e10c9a19f2710abe1c319
Rajiv Shah <rajivshah3@icloud.com>
2017-08-25 18:33:47 -0400

    Add reattach.js

---
+++ reattach.js
@@ -0,0 +1,6 @@
+var IOTA = require('/Users/rajiv/iota.lib.js/lib/iota');
+// Create IOTA instance with host and port as provider
+var iota = new IOTA({
+  'host': 'http://rajiv-shah.ddns.net',
+  'port': 14265
+});
