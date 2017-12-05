e672ee4b8259650a8ea4188840bad8ef9e1fa4cd
Rajiv Shah <rajivshah1@icloud.com>
2017-12-04 21:42:50 -0500

    Update files

--- reattach.js
+++ reattach.js
@@ -1,7 +1,7 @@
-var IOTA = require('./lib/iota');
+var IOTA = require('./node_modules/iota.lib.js');
 // Create IOTA instance with host and port as provider
 var iota = new IOTA({
-  'host': 'http://35.196.142.70',
+  'host': 'http://iotanode.us:443',
   'port': 443
 });
 
@@ -9,23 +9,8 @@ var iota = new IOTA({
 var txHash = "";
 
 
-function reattachTx(txHash){
-var quote = "'"
-var quoted = quote.concat(txHash);
-var quotedTxHash = quoted.concat(quote);
-document.getElementById("status").innerHTML = "Reattaching..."
-iota.api.replayBundle(quotedTxHash,
- 3, 15, function(e,s){console.log(s)});
-
-}
-
-
-function setTxHash(txHash) {
-  txHash = document.getElementById('reattachTxHash');
-  reattachTx(txHash);
+global.reattachTx = function (txHash){
+document.getElementById("status").innerHTML = "Reattaching...";
+iota.api.replayBundle(txHash,
+ 3, 14, function(e,s){console.log(s)});
 };
-
-function onSubmit(token) {
-  console.log("Success");
-    setTxHash('reattachTxHash');
-  };
