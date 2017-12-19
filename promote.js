efa663d5b6ed3498ed93ed373b34c922ed6ea0a9
Rajiv Shah <rajivshah1@icloud.com>
2017-12-19 12:39:18 -0500

    Enable promotion

---
+++ promote.js
@@ -0,0 +1,29 @@
+var IOTA = require('iota.lib.js');
+// Create IOTA instance with host and port as provider
+var iota = new IOTA({
+  'host': 'https://iotanode.us',
+  'port': 443
+});
+
+
+
+global.promoteTx = function (txHash){
+  document.getElementById("status").innerHTML = "Promoting...";
+  const transfer = [{
+    address: "RAJIV9PROMOTER9999999999999999999999999999999999999999999999999999999999999999999",
+    value: 0,
+    message: "CCFDPCBDGDPCRCHDXCCDBDEADDFDCDADCDHDTCFDEAPCJDPCXC9DPCQC9DTCEAPCHDEAWCHDHDDDGDDBTATAFDPCYCXCJDGDWCPCWCXASAVCXCHDWCIDQCSAXCCDTAXCCDHDPCRADDCDKDRATCFDTADDFDCDADCDHDTCSAWCHDAD9D",
+    tag: "RAJIV9PROMOTER"
+  }];
+
+  iota.api.promoteTransaction(txHash, 3, 14, transfer, { interrupt: false, delay: 0 }, function(e, s){
+    if(s){
+      document.getElementById("status").innerHTML = "Success!";
+      console.log(s);
+    }
+    if(e){
+      document.getElementById("status").innerHTML = "Error";
+      console.log(s);
+    }
+  });
+}
